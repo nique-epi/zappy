2c47374d7886e4815576f225a4673d097d1eedef
Yannislrg <yannis.lorang@epitech.eu>
2026-06-04 14:29:10 +0200

    feat: update SonarQube configuration and enhance main.py documentation

--- ia/main.py
+++ ia/main.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+"""Entry point for the Zappy AI."""
+
 
 def main():
+    """Main function for the Zappy AI."""
     print("Zappy AI")
+
 
 if __name__ == "__main__":
     main()
